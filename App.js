fd49705c794cbe5e8655f7df5f9f2ff56934bd46
Justin Urbanski <jurbanski@jurbanski-mbr.local>
2014-05-27 08:49:07 +1000

    Finished combo boxes

--- App.js
+++ App.js
@@ -6,37 +6,107 @@ Ext.define('CustomApp', {
 
     launch: function() {
         console.log("Justin's first Rally app!");
+        
+        this.pulldownContainer = Ext.create('Ext.container.Container', {
+        	id: 'pulldown-container-id',
+        	layout: {
+        		type: 'hbox',
+        		align: 'stretch'
+        	},
+        });
+        
+        this.add(this.pulldownContainer);
 
-		this._loadData();
+		this._loadIterations();
+    },
+    
+    _loadIterations: function() {
+    	this.iterComboBox = Ext.create('Rally.ui.combobox.IterationComboBox', {
+    		fieldLabel: 'Iteration',
+    		labelAlign: 'right',
+    		width: 300,
+    		listeners: {
+    			ready: function(comboBox) {
+    				this._loadPriorities();
+    			},
+    			select: function(comboBox, records) {
+    				this._loadData();
+    			},
+    			scope: this
+    		},
+    	});
+    	
+    	this.pulldownContainer.add(this.iterComboBox);
+    },
+    
+    _loadPriorities: function() {
+    	this.priComboBox = Ext.create('Rally.ui.combobox.FieldValueComboBox', {
+    		model: 'User Story',
+    		field: 'Priority',
+    		fieldLabel: 'Priority',
+    		labelAlign: 'right',
+    		listeners: {
+    			ready: function(comboBox) {
+    				this._loadData();
+    			},
+    			select: function(comboBox, records) {
+    				this._loadData();
+    			},
+    			scope: this
+    		},
+    	});
+    	
+    	this.pulldownContainer.add(this.priComboBox);
     },
     
     _loadData: function() {
-		var myStore = Ext.create('Rally.data.wsapi.Store', {
-				model: 'User Story',
-			autoLoad: true,
-			listeners: {
-				load: function(myStore, myData, success) {
-					console.log('Got data!', myStore, myData, success);	
-					this._loadGrid(myStore);
+    	var selectedIterRef = this.iterComboBox.getRecord().get('_ref');
+    	var selectedPriValue = this.priComboBox.getRecord().get('value');
+    	
+    	var myFilters = [
+				{
+					property: 'Iteration',
+					operation: '=',
+					value: selectedIterRef
 				},
-			scope: this
-			},
-			fetch: ['FormattedID', 'Name', 'ScheduleState']
-		});
+				{
+					property: 'Priority',
+					operation: '=',
+					value: selectedPriValue
+				}
+			];
+    	
+    	// If store exists, just reload new data
+    	if (this.userStoryStore) {
+    		this.userStoryStore.setFilter(myFilters);
+    		this.userStoryStore.load();
+    	} else {
+			this.userStoryStore = Ext.create('Rally.data.wsapi.Store', {
+				model: 'User Story',
+				autoLoad: true,
+				filter: myFilters,
+				listeners: {
+					load: function(myStore, myData, success) {
+						console.log('Got data!', myStore, myData, success);	
+						if (!this.myGrid) {
+							this._createGrid(myStore);
+						}
+					},
+				scope: this
+				},
+				fetch: ['FormattedID', 'Name', 'Priority', 'ScheduleState']
+			});
+		}
     },
     
-	_loadGrid: function(myStoryStore) {
-		var myGrid = Ext.create('Rally.ui.grid.Grid', {
+	_createGrid: function(myStoryStore) {
+		this.myGrid = Ext.create('Rally.ui.grid.Grid', {
 			store: myStoryStore,
 			columnCfgs: [
-				'FormattedID', 'Name', 'ScheduleState'
+				'FormattedID', 'Name', 'Priority', 'ScheduleState'
 			]
 		});
 
-		console.log('my grid', myGrid);
-
-		this.add(myGrid);
-
-		console.log('What is this?', this);
+		this.add(this.myGrid);
     }
 });
