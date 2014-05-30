a9c92501e83ea60a3da4403babac0ec456002be7
Justin Urbanski <jurbanski@jurbanski-mbr.local>
2014-05-30 11:29:42 +1000

    Trying to create custom column in grid

--- App.js
+++ App.js
@@ -2,79 +2,82 @@ Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
 
-    // items:{ html:'<a href="https://help.rallydev.com/apps/2.0rc2/doc/">App SDK 2.0rc2 Docs</a>'},
+    items: [
+    	{
+			xtype: 'container',
+			itemId: 'pulldown-container',
+			layout: {
+				type: 'hbox',
+				align: 'stretch'
+			}
+		}
+    ],
 
     launch: function() {
         console.log("Justin's first Rally app!");
         
-        this.pulldownContainer = Ext.create('Ext.container.Container', {
-        	id: 'pulldown-container-id',
-        	layout: {
-        		type: 'hbox',
-        		align: 'stretch'
-        	},
-        });
+        var currentProject =  Rally.environment.getContext().getProject();
+        var workspace = Rally.environment.getContext().getWorkspace();
+ 		console.log(currentProject, workspace);
         
-        this.add(this.pulldownContainer);
-
 		this._loadIterations();
     },
     
     _loadIterations: function() {
-    	this.iterComboBox = Ext.create('Rally.ui.combobox.IterationComboBox', {
+    	var me = this;
+    	var iterComboBox = Ext.create('Rally.ui.combobox.IterationComboBox', {
+    		itemId: 'iteration-combobox',
     		fieldLabel: 'Iteration',
     		labelAlign: 'right',
     		width: 300,
     		listeners: {
-    			ready: function(comboBox) {
-    				this._loadPriorities();
-    			},
-    			select: function(comboBox, records) {
-    				this._loadData();
-    			},
-    			scope: this
+    			ready: me._loadPriorities,
+    			select: me._loadData,
+    			scope: me
     		},
     	});
     	
-    	this.pulldownContainer.add(this.iterComboBox);
+    	this.down('#pulldown-container').add(iterComboBox);
     },
-    
+
     _loadPriorities: function() {
-    	this.priComboBox = Ext.create('Rally.ui.combobox.FieldValueComboBox', {
+    	var priComboBox = Ext.create('Rally.ui.combobox.FieldValueComboBox', {
+    		itemId: 'priority-combobox',
     		model: 'User Story',
     		field: 'Priority',
     		fieldLabel: 'Priority',
     		labelAlign: 'right',
     		listeners: {
-    			ready: function(comboBox) {
-    				this._loadData();
-    			},
-    			select: function(comboBox, records) {
-    				this._loadData();
-    			},
+    			ready: this._loadData,
+    			select: this._loadData,
     			scope: this
     		},
     	});
     	
-    	this.pulldownContainer.add(this.priComboBox);
+    	this.down('#pulldown-container').add(priComboBox);
+    },
+    
+    _getFilters: function(iterValue, priValue) {
+    	var iterFilter = Ext.create('Rally.data.wsapi.Filter', {
+    	    property: 'Iteration',
+			operation: '=',
+			value: iterValue
+    	});
+    	
+    	var priFilter = Ext.create('Rally.data.wsapi.Filter', {
+    	    property: 'Priority',
+			operation: '=',
+			value: priValue
+    	});
+    	
+    	return iterFilter.and(priFilter);
     },
     
     _loadData: function() {
-    	var selectedIterRef = this.iterComboBox.getRecord().get('_ref');
-    	var selectedPriValue = this.priComboBox.getRecord().get('value');
+    	var selectedIterRef = this.down('#iteration-combobox').getRecord().get('_ref');
+    	var selectedPriValue = this.down('#priority-combobox').getRecord().get('value');
     	
-    	var myFilters = [
-				{
-					property: 'Iteration',
-					operation: '=',
-					value: selectedIterRef
-				},
-				{
-					property: 'Priority',
-					operation: '=',
-					value: selectedPriValue
-				}
-			];
+    	var myFilters = this._getFilters(selectedIterRef, selectedPriValue);
     	
     	// If store exists, just reload new data
     	if (this.userStoryStore) {
@@ -87,26 +90,78 @@ Ext.define('CustomApp', {
 				filter: myFilters,
 				listeners: {
 					load: function(myStore, myData, success) {
-						console.log('Got data!', myStore, myData, success);	
+						console.log('Got data!', myStore, myData, success);
+						
+						Ext.Array.each(myData, function(data) {
+							data.set('Justin', 1);
+						});
+						
+						console.log('After Justin data!', myData);
+						
 						if (!this.myGrid) {
 							this._createGrid(myStore);
 						}
+						
+						this._loadSnapshot(myData, myFilters);
 					},
-				scope: this
+					scope: this
 				},
-				fetch: ['FormattedID', 'Name', 'Priority', 'ScheduleState']
+				fetch: ['FormattedID', 'Name', 'Priority', 'ScheduleState', 'CreationDate', 'Justin']
 			});
 		}
     },
     
 	_createGrid: function(myStoryStore) {
+	
+		console.log('in create!', myStoryStore);
+		
 		this.myGrid = Ext.create('Rally.ui.grid.Grid', {
 			store: myStoryStore,
 			columnCfgs: [
-				'FormattedID', 'Name', 'Priority', 'ScheduleState'
-			]
+				{
+                    text: 'Formatted ID', dataIndex: 'FormattedID', xtype: 'templatecolumn',
+                    tpl: Ext.create('Rally.ui.renderer.template.FormattedIDTemplate')
+                },
+                {
+                    text: 'Name', dataIndex: 'Name'
+                },
+                {
+                    text: 'Priority', dataIndex: 'Priority'
+                },
+                {
+                    text: 'Justin', dataIndex: 'Justin', xtype: 'templatecolumn'
+                }
+			],
 		});
 
 		this.add(this.myGrid);
-    }
+    },
+    
+    _loadSnapshot: function(myData1, myFilters1) {
+    	console.log('Before', myData1, myFilters1);
+    	var snapshotStore = Ext.create('Rally.data.lookback.SnapshotStore', {
+			context: {
+				workspace: '/workspace/41529001',
+				project: '/project/19196251073',
+				projectScopeUp: false,
+				projectScopeDown: false
+			},
+			filters: [
+				{
+					property: 'FormattedID',
+					operation: '=',
+					value: 'S67202'
+				}
+			],
+    		autoLoad: true,
+    		listeners: {
+        		load: function(store, records) {
+            		console.log('Snapshots: ', records[0].get('Blocked'));
+        		},
+        		scope: this
+    		},
+    		fetch: ['FormattedID', 'Name', 'ScheduleState', 'Revisions', 'Blocked', 'CreationDate'],
+		});
+	},
+	
 });
